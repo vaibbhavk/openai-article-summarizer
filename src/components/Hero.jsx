3bbbc241cff947c6bf36617adbfd6c213378f19a
Vaibhav Kesharwani <vaibhav.vk2128@gmail.com>
2023-04-24 14:17:49 +0530

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center flex-col">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button type="button" onClick={()=>{window.open("https://")}}>GitHub</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Hero;
